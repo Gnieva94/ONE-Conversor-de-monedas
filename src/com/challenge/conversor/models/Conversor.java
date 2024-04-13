9a3d955a4a02ae470d39f01cceaacaaa3605c3d0
Gnieva94 <gus_94@hotmail.com.ar>
2024-04-13 03:53:36 -0300

    comment some methods.

--- src/com/challenge/conversor/models/Conversor.java
+++ src/com/challenge/conversor/models/Conversor.java
@@ -10,11 +10,11 @@ public class Conversor {
         this.conversion_rate = cr.conversion_rate();
     }
     public String getBase_code() {return base_code;}
-    public void setBase_code(String base_code) {this.base_code = base_code;}
+    //public void setBase_code(String base_code) {this.base_code = base_code;}
     public String getTarget_code() {return target_code;}
-    public void setTarget_code(String target_code) {this.target_code = target_code;}
-    public double getConversion_rate() {return conversion_rate;}
-    public void setConversion_rate(double conversion_rate) {this.conversion_rate = conversion_rate;}
+    //public void setTarget_code(String target_code) {this.target_code = target_code;}
+    //public double getConversion_rate() {return conversion_rate;}
+    //public void setConversion_rate(double conversion_rate) {this.conversion_rate = conversion_rate;}
     public double Conversion(double base){
         return base * this.conversion_rate;
     }
